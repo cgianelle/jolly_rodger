15c36455de9cf2b8037eac6e1d6be7db97ef2071
Chris Gianelle <cgianelle@cipherhealth.com>
2021-06-02 18:04:24 -0400

    Added more features to jolly roger. Also added linting

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 class JollyRoger {
-    //--Map flag configuration internally
-    constructor(flagConfiguration) {
-        this.flags = flagConfiguration
-    }
+  // --Map flag configuration internally
+  constructor (flagConfiguration = {}) {
+    this.featureFlags = JSON.parse(JSON.stringify(flagConfiguration))
+  }
 
-    featureIsEnabled(featureName) {
-        return this.flags[featureName];
-    }
+  // --If featureName is not found in the map, then it will return undefined
+  isFeatureEnabled (featureName) {
+    return this.featureFlags[featureName]
+  }
+
+  setFeature (featureName, isFeatureEnabled) {
+    this.featureFlags[featureName] = isFeatureEnabled
+  }
+
+  removeFeature (featureName) {
+    return featureName in this.featureFlags ? delete this.featureFlags[featureName] : false
+  }
 }
 
-exports.JollyRoger = JollyRoger;
+exports.JollyRoger = JollyRoger
--- test/test_jolly_roger.js
+++ test/test_jolly_roger.js
@@ -1,10 +1,67 @@
-const {JollyRoger} = require('../index');
-const {expect, assert} = require('chai');
+/* eslint-disable no-unused-expressions */
+const { JollyRoger } = require('../index')
+const { expect } = require('chai')
 
-describe('Jolly Roger Feature Flag Tests', ()=> {
-    it('Should create the JollyRoger class and validate ', ()=> {
-        const theJollyRoger = new JollyRoger({'test': false, 'test2': true});
-        expect(theJollyRoger.featureIsEnabled('test')).to.be.false;
-        expect(theJollyRoger.featureIsEnabled('test2')).to.be.true;
-    });
-});
+describe('Jolly Roger Feature Flag Tests', () => {
+  it('Should create the JollyRoger class and validate ', () => {
+    const theJollyRoger = new JollyRoger({ test: false, test2: true })
+    expect(theJollyRoger.isFeatureEnabled('test')).to.be.false
+    expect(theJollyRoger.isFeatureEnabled('test2')).to.be.true
+  })
+
+  it('What will happen if I forget to pass in a config block?', () => {
+    const theJollyRoger = new JollyRoger()
+    expect(theJollyRoger.isFeatureEnabled('test')).to.be.undefined
+  })
+
+  it('What will happen if I pass something other than a map into the class?', () => {
+    const theJollyRoger = new JollyRoger('A String')
+    expect(theJollyRoger.isFeatureEnabled('test')).to.be.undefined
+  })
+
+  it('Should not find anything when asked for a feature that has not been registered', () => {
+    const theJollyRoger = new JollyRoger({})
+    expect(theJollyRoger.isFeatureEnabled('test')).to.be.undefined
+  })
+
+  it('The value of a flag should be consistent with the value it had when creating an instance', () => {
+    const config = { test: false }
+    const theJollyRoger = new JollyRoger(config)
+    config.test = true
+
+    // --what will jolly roger say is the state of test?
+    expect(theJollyRoger.isFeatureEnabled('test')).to.be.false
+  })
+
+  it('Should change the assigned value of a feature flag', () => {
+    const theJollyRoger = new JollyRoger({ test: false, test2: true })
+    theJollyRoger.setFeature('test', true)
+    theJollyRoger.setFeature('test2', false)
+
+    expect(theJollyRoger.isFeatureEnabled('test')).to.be.true
+    expect(theJollyRoger.isFeatureEnabled('test2')).to.be.false
+  })
+
+  it('Should add a new feature and set its value', () => {
+    const theJollyRoger = new JollyRoger({ test: false, test2: true })
+    theJollyRoger.setFeature('test3', true)
+    expect(theJollyRoger.isFeatureEnabled('test3')).to.be.true
+  })
+
+  it('Should remove a feature', () => {
+    const theJollyRoger = new JollyRoger({ test: false, test2: true })
+    expect(theJollyRoger.removeFeature('test')).to.be.true
+    expect(theJollyRoger.isFeatureEnabled('test')).to.be.undefined
+  })
+
+  it('Should be able to remove a feature and not affect any other features', () => {
+    const theJollyRoger = new JollyRoger({ test: false, test2: true })
+    expect(theJollyRoger.removeFeature('test')).to.be.true
+    expect(theJollyRoger.isFeatureEnabled('test2')).to.be.true
+  })
+
+  it('Should not be able to delete anything if it does not exist', () => {
+    const theJollyRoger = new JollyRoger({ test: false, test2: true })
+    expect(theJollyRoger.removeFeature('test3')).to.be.false
+  })
+})
